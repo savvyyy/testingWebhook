78a07e7522c480cba97d7780bec3d38a5d04c922
AdityaKumarPrajapati <aditya.prajapati@successive.tech>
2020-01-06 14:09:06 +0530

    server commit

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-console.log('Aditya')
-console.log('Aditya')
-console.log('Aditya')
-console.log('Aditya')
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('<b>Hello Aditya</b>')
+})
+
+app.listen(3001, function() {
+    console.log('App is listening to port 3001')
+})
